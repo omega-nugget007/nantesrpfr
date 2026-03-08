4da443ba3fa35ead4575a68dd33df17c912e6d9b
omega-nugget007 <maxi.emy8@gmail.com>
2026-03-08 01:05:49 +0100

    Update staffCandidature.js

--- js/staffCandidature.js
+++ js/staffCandidature.js
@@ -2,19 +2,24 @@ document.getElementById("staffForm").addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const data = {
-        prenom: prenom.value,
-        age: age.value,
-        motivations: motivations.value,
-        experience: experience.value,
-        email: email.value
+        prenom: document.getElementById("prenom").value,
+        age: document.getElementById("age").value,
+        motivations: document.getElementById("motivations").value,
+        experience: document.getElementById("experience").value,
+        email: document.getElementById("email").value
     };
 
-    const webhook = "URL_WEBHOOK_STAFF";
+    const webhook = "https://script.google.com/macros/s/AKfycbwu975F5upHCZShYw5Ht6mlrr_UWKr7AV0yFYx4Goe9jqKaxwtsTdHJpE8Tw0bTJ2rQ/exec";
 
-    await fetch(webhook, {
-        method: "POST",
-        body: JSON.stringify(data)
-    });
+    try {
+        await fetch(webhook, {
+            method: "POST",
+            body: JSON.stringify(data)
+        });
 
-    alert("Candidature envoyée !");
+        alert("Candidature staff envoyée !");
+    } catch (err) {
+        alert("Erreur lors de l’envoi.");
+        console.error(err);
+    }
 });
